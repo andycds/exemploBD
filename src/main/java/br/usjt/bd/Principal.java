906a2f02b7dc8c2deadf875995618623d00747a0
Anderson Sanches <andycds@gmail.com>
2020-11-03 20:38:16 -0300

    Outros exercicios

--- src/main/java/br/usjt/bd/Principal.java
+++ src/main/java/br/usjt/bd/Principal.java
@@ -4,7 +4,10 @@ import javax.swing.JOptionPane;
 
 public class Principal {
 	public static void main(String[] args) {
-		String menu = "1-Cadastrar\n2-Atualizar\n3-Apagar\n4-Listar\n0-Sair";
+		// 1.1 Exibir os dados de uma pessoa, dado seu código.
+		// 1.2 Apagar os dados de uma pessoa, dado seu e-mail.
+		String menu = "1 - Cadastrar\n" + "2 - Atualizar\n" + "3 - Apagar\n" + "4 - Listar\n"
+				+ "5 - Exibir dados de uma pessoa\n" + "6 - Apagar pessoa por email\n" + "0 - Sair";
 		String nome;
 		String fone;
 		String email;
@@ -43,6 +46,20 @@ public class Principal {
 				p.apagar();
 				break;
 			case 4:
+				p = new Pessoa();
+				p.listar();
+				break;
+			case 5: // Exibir dados de uma pessoa
+				codigo = Integer.parseInt(JOptionPane.showInputDialog("Codigo?"));
+				p = new Pessoa();
+				p.setCodigo(codigo);
+				p.listarPessoaPorCodigo();
+				break;
+			case 6: // Apagar pessoa por email
+				email = JOptionPane.showInputDialog("Email?");
+				p = new Pessoa();
+				p.setEmail(email);
+				p.apagarPorEmail();
 				break;
 			case 0:
 				break;
